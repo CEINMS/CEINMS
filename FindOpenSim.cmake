ca96865b449ce944ed9d12840f720f6e45912404
Elena Ceseracciu <eowyn83@gmail.com>
2014-03-10 14:05:04 +0100

    Have FindOpensim.cmake actually find the libraries
    (platform-specific libs may be missing)

--- FindOpenSim.cmake
+++ FindOpenSim.cmake
@@ -21,82 +21,105 @@ MESSAGE( STATUS "OpenSim Include path: ${OPENSIM_INCLUDE_DIRS}")
 
 find_path(SIMTK_INCLUDE_DIRS Simbody.h
           PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
-          PATH_SUFFIXES sdk/include/SimTK/include)
+          PATH_SUFFIXES sdk/include/SimTK/include sdk/include/SimTK/simbody)
 MESSAGE( STATUS "SimTK Include path: ${SIMTK_INCLUDE_DIRS}")
 		  
-find_path(OPENSIM_LIBS_DIR 
-		  NAMES
-			  osimCommon.lib
-			  osimCommon.so
-			  libosimCommon.so
-          PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
-          PATH_SUFFIXES sdk/lib lib)
-MESSAGE( STATUS "OpenSim Lib path: ${OPENSIM_LIBS_DIR}")
+#find_path(OPENSIM_LIBS_DIR
+#		  NAMES
+#			  osimCommon.lib
+#			  osimCommon.so
+#			  libosimCommon.so
+#          PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
+#          PATH_SUFFIXES sdk/lib lib)
+#MESSAGE( STATUS "OpenSim Lib path: ${OPENSIM_LIBS_DIR}")
 		  
-
+#why?
 find_path(OPENSIM_DLL_DIR 
 		  NAMES
 			  osimCommon.dll
-			  id
+#			  id
           PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
           PATH_SUFFIXES bin)
 MESSAGE( STATUS "OpenSim dll path: ${OPENSIM_DLL_DIR}")	  
 
-SET(OPENSIM_LIBRARY_DIRS ${OPENSIM_LIBS_DIR} ${OPENSIM_DLL_DIR})
-MESSAGE( STATUS "OpenSim Library path: ${OPENSIM_LIBRARY_DIRS}")
-
-
-SET(NameSpace OpenSim_)
-SET(OPENSIM_ALL_LIBS
-osimSimulation
-osimActuators
-osimCommon
-osimAnalyses
-osimTools
-${NameSpace}SimTKcommon
-${NameSpace}SimTKmath
-${NameSpace}SimTKsimbody
-SimTKlapack
-${PLATFORM_LIBS}
-)
+#SET(OPENSIM_LIBRARY_DIRS ${OPENSIM_LIBS_DIR} ${OPENSIM_DLL_DIR})
+#MESSAGE( STATUS "OpenSim Library path: ${OPENSIM_LIBRARY_DIRS}")
 
 if(UNIX)
-SET(NameSpace )
-SET(OPENSIM_ALL_LIBS
-osimSimulation
-osimActuators
-osimCommon
-osimAnalyses
-osimTools
-${NameSpace}SimTKcommon
-${NameSpace}SimTKmath
-${NameSpace}SimTKsimbody
-${PLATFORM_LIBS}
-)
-endif(UNIX)
+    set(NameSpace )
+else()
+    set(NameSpace OpenSim_)
+endif()
 
+find_library(osimSimulation
+             NAMES osimSimulation
+             PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
+             PATH_SUFFIXES sdk/lib lib)
 
+find_library(osimCommon
+             NAMES osimCommon
+             PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
+             PATH_SUFFIXES sdk/lib lib)
 
-IF(WIN32)
-	SET(PLATFORM_LIBS  pthreadVC2)
-ELSE (WIN32)
-  SET(NameSpace "")
-  IF(APPLE)
-	SET(PLATFORM_LIBS  SimTKAtlas)
-  ELSE(APPLE)
-	SET(PLTAFORM_LIBS SimTKAtlas_Lin_generic)
-  ENDIF(APPLE)
-ENDIF (WIN32)
+find_library(osimActuators
+             NAMES osimActuators
+             PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
+             PATH_SUFFIXES sdk/lib lib)
+
+find_library(osimAnalyses
+             NAMES osimAnalyses
+             PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
+             PATH_SUFFIXES sdk/lib lib)
+
+find_library(osimTools
+             NAMES osimTools
+             PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
+             PATH_SUFFIXES sdk/lib lib)
+
+find_library(${NameSpace}SimTKcommon
+             NAMES ${NameSpace}SimTKcommon
+             PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
+             PATH_SUFFIXES sdk/lib lib)
+
+find_library(${NameSpace}SimTKmath
+             NAMES ${NameSpace}SimTKmath
+             PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
+             PATH_SUFFIXES sdk/lib lib)
+
+find_library(${NameSpace}SimTKsimbody
+             NAMES ${NameSpace}SimTKsimbody
+             PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
+             PATH_SUFFIXES sdk/lib lib)
+#does this exist?
+#find_library(SimTKlapack
+#             NAMES SimTKlapack
+#             PATHS ${OPENSIM_POSSIBLE_DIRECTORIES}
+#             PATH_SUFFIXES sdk/lib lib)
+
+set(OPENSIM_LIBRARIES ${osimSimulation} ${osimCommon} ${osimActuators} ${osimAnalyses} ${osimTools} ${${NameSpace}SimTKcommon} ${${NameSpace}SimTKmath} ${${NameSpace}SimTKsimbody})
+
+message("opensim libs: ${OPENSIM_LIBRARIES}")
+
+#IF(WIN32)
+#	SET(PLATFORM_LIBS  pthreadVC2)
+#ELSE (WIN32)
+#  SET(NameSpace "")
+#  IF(APPLE)
+#	SET(PLATFORM_LIBS  SimTKAtlas)
+#  ELSE(APPLE)
+#	SET(PLATFORM_LIBS SimTKAtlas_Lin_generic)
+#  ENDIF(APPLE)
+#ENDIF (WIN32)
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set OPENSIM_FOUND to TRUE
 # if all listed variables are TRUE
 # DEFAULT_MSG is predefined... change only if you need a custom msg
 find_package_handle_standard_args(OpenSim DEFAULT_MSG
-                                  OPENSIM_LIBRARY_DIRS OPENSIM_INCLUDE_DIRS SIMTK_INCLUDE_DIRS)
+                                  OPENSIM_LIBRARIES OPENSIM_INCLUDE_DIRS SIMTK_INCLUDE_DIRS)
 
 # Mark the following variables as advanced (they are not displayed in any 
 # of the cmake GUIs unless the show advanced option is on.
-mark_as_advanced(LIBXML2_INCLUDE_DIR LIBXML2_LIBRARY )
+#mark_as_advanced(LIBXML2_INCLUDE_DIR LIBXML2_LIBRARY )
 
 
