63a0f08c740c2ba77c39060edfd07b7112a76181
Elena Ceseracciu <eowyn83@gmail.com>
2014-03-19 19:16:07 +0100

    Fixed CMake files after testing on windows (64 bit compilation)

--- FindXERCES.cmake
+++ FindXERCES.cmake
@@ -14,8 +14,9 @@ FIND_PATH(XERCES_INCLUDE_DIR /xercesc/util/XercesVersion.hpp
  
 MESSAGE(STATUS "XERCES Include Dir ${XERCES_INCLUDE_DIR}")
  
-FIND_LIBRARY(XERCES_LIBRARY
-  NAMES  
+if(CMAKE_SIZEOF_VOID_P EQUAL 8) #64bit compilation
+    FIND_LIBRARY(XERCES_LIBRARY
+  NAMES
       xerces-c
       xerces-c_2
 	  xerces-c_3
@@ -24,11 +25,28 @@ FIND_LIBRARY(XERCES_LIBRARY
   PATHS 
     "${XERCES_DIR}\\lib"
     /usr/local/lib
-    /usr/lib 
-    /usr/lib/x86_64-linux-gnu/ 
+    /usr/lib
+    /usr/lib/x86_64-linux-gnu/
+	"C:/Program\ Files\ (x86)/CodeSynthesis\ XSD\ 3.3/bin64"
+	"C:/Program\ Files\ (x86)/CodeSynthesis\ XSD\ 3.3/lib64/vc-10.0"
+  )
+else()
+    FIND_LIBRARY(XERCES_LIBRARY
+  NAMES
+      xerces-c
+      xerces-c_2
+	  xerces-c_3
+	  xerces-c_3_1
+	  xerces-c_3_1_vc100
+  PATHS
+    "${XERCES_DIR}\\lib"
+    /usr/local/lib
+    /usr/lib
+    /usr/lib/x86_64-linux-gnu/
 	"C:/Program\ Files\ (x86)/CodeSynthesis\ XSD\ 3.3/bin"
 	"C:/Program\ Files\ (x86)/CodeSynthesis\ XSD\ 3.3/lib/vc-10.0"
   )
+endif()
 
   MESSAGE(STATUS "XERCES Lib Dir ${XERCES_LIBRARY}")
   
