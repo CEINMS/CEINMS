683a61ee78829fb3c26312d3deec5a0ee7ec0023
Claudio Pizzolato <claudio.pizzolato@griffithuni.edu.au>
2014-08-25 19:00:47 +0200

    Add floating point comparison, calibration undersamples emgs

--- lib/Calibration/TorqueComputationModes/ComputationMode_Fast.cpp
+++ lib/Calibration/TorqueComputationModes/ComputationMode_Fast.cpp
@@ -1,7 +1,8 @@
 #include <vector>
 using std::vector;
 #include "TrialData.h"
-#define DEBUG
+#include "NumCompare.h"
+//#define DEBUG
 
 template<typename NMSmodelT>
 ComputationMode_Fast<NMSmodelT>::ComputationMode_Fast(NMSmodelT& subject):
@@ -82,10 +83,12 @@ void ComputationMode_Fast<NMSmodelT>::computeTorques(vector< vector< std::vector
 
         for (int i = 0; i < trials_.at(ct).noEmgSteps_; ++i) {
             double emgTime = trials_.at(ct).emgTimeSteps_.at(i) + subject_.getGlobalEmDelay();
-            if(emgTime < lmtTime) 
-                subject_.setTime_emgs_updateActivations_pushState_selective(emgTime, trials_.at(ct).emgData_.at(i), musclesToUpdate_);
 
-            if ( (lmtTime <= emgTime) && (k < trials_.at(ct).noLmtSteps_)) {
+// No more emg without corresponding lmt
+//            if(emgTime < lmtTime) 
+//                subject_.setTime_emgs_updateActivations_pushState_selective(emgTime, trials_.at(ct).emgData_.at(i), musclesToUpdate_);
+
+            if (NumCompare::lessEqual(lmtTime, emgTime) && (k < trials_.at(ct).noLmtSteps_) && (k < trials_.at(ct).maData_.front().size())) {
                 subject_.setMuscleForces(forceDataT1_.at(ct).at(k));
                 subject_.setTime(emgTime);
                 subject_.setEmgsSelective(trials_.at(ct).emgData_.at(i), musclesToUpdate_);
@@ -103,9 +106,6 @@ void ComputationMode_Fast<NMSmodelT>::computeTorques(vector< vector< std::vector
                 for (int j = 0; j < subject_.getNoMuscles(); ++j)
                     forceDataT1_.at(ct).at(k).at(j) = currentForces.at(j);
             
-                ++k;
-                if (k < trials_.at(ct).noLmtSteps_)
-                    lmtTime = trials_.at(ct).lmtTimeSteps_.at(k);  
 
 #ifdef DEBUG
                 cout << endl << endl << "EmgTime: " << emgTime << endl << "EMG" << endl;
@@ -124,6 +124,10 @@ void ComputationMode_Fast<NMSmodelT>::computeTorques(vector< vector< std::vector
         
                 cout << endl << "----------------------------------------" << endl;
 #endif
+                ++k;
+                if (k < trials_.at(ct).noLmtSteps_)
+                    lmtTime = trials_.at(ct).lmtTimeSteps_.at(k);
+
             }
             
         } 
@@ -140,23 +144,24 @@ void ComputationMode_Fast<NMSmodelT>::computeTorquesAndPenalties(vector< vector<
     getMusclesToUpdate();
     // i is for the number of trials
     for (unsigned int ct = 0 ; ct < trials_.size(); ++ct) {
+        std::cout << "Trial " << ct + 1 << "/" << trials_.size() << std::endl;
         initFiberLengthTraceCurves(ct);
         unsigned k = 0; // k is the index for lmt and ma data
         double lmtTime = trials_.at(ct).lmtTimeSteps_.at(k);
 
         for (int i = 0; i < trials_.at(ct).noEmgSteps_; ++i) {
             double emgTime = trials_.at(ct).emgTimeSteps_.at(i) + subject_.getGlobalEmDelay();
-            if(emgTime < lmtTime) 
-                subject_.setTime_emgs_updateActivations_pushState_selective(emgTime, trials_.at(ct).emgData_.at(i), musclesToUpdate_);
+// No more emg without corresponding lmt
+//            if(emgTime < lmtTime) 
+//                subject_.setTime_emgs_updateActivations_pushState_selective(emgTime, trials_.at(ct).emgData_.at(i), musclesToUpdate_);
 
-            if ( (lmtTime <= emgTime) && (k < trials_.at(ct).noLmtSteps_)) {
+            if (NumCompare::lessEqual(lmtTime, emgTime) && (k < trials_.at(ct).noLmtSteps_) && (k < trials_.at(ct).maData_.front().size())) {
                 subject_.setMuscleForces(forceDataT1_.at(ct).at(k));
                 subject_.setTime(emgTime);
                 subject_.setEmgsSelective(trials_.at(ct).emgData_.at(i), musclesToUpdate_);
                 subject_.setMuscleTendonLengths(trials_.at(ct).lmtData_.at(k));
                 for (int j = 0 ; j < trials_.at(ct).noDoF_; ++j)
                     subject_.setMomentArms(trials_.at(ct).maData_.at(j).at(k), j);
-   
                 subject_.updateState_OFFLINE(musclesToUpdate_);
                 subject_.pushState(musclesToUpdate_);
                 vector<double> currentTorques, currentForces;
@@ -176,27 +181,28 @@ void ComputationMode_Fast<NMSmodelT>::computeTorquesAndPenalties(vector< vector<
                 for(unsigned j = 0; j < subject_.getNoMuscles(); ++j)
                     penalties.at(ct).at(k) += penaltyDataT1_.at(ct).at(k).at(j);
                 //fine calcolo penalties
+           
+#ifdef DEBUG
+                cout << endl << endl << "EmgTime: " << emgTime << endl << "EMG" << endl;
+
+                for (unsigned int l = 0; l < trials_.at(ct).emgData_.at(i).size(); ++l)
+                    cout << trials_.at(ct).emgData_.at(i).at(l) << "\t";
+                cout << endl << "LmtTime: " << lmtTime << endl;
+
+                for (unsigned int l = 0; l < trials_.at(ct).lmtData_.at(k).size(); ++l)
+                    cout << trials_.at(ct).lmtData_.at(k).at(l) << "\t";
+                cout << endl << "MomentArms" << endl;
+
+                for (unsigned int l = 0; l < trials_.at(ct).maData_.at(0).at(k).size(); ++l)
+                    cout << trials_.at(ct).maData_.at(0).at(k).at(l) << "\t";
+                cout << "\ncurrent torque: " << currentTorques.at(0);
+
+                cout << endl << "----------------------------------------" << endl;
+#endif
                 ++k;
                 if (k < trials_.at(ct).noLmtSteps_)
                     lmtTime = trials_.at(ct).lmtTimeSteps_.at(k);  
-
-#ifdef DEBUG
-                cout << endl << endl << "EmgTime: " << emgTime << endl << "EMG" << endl;
-        
-                for(unsigned int l=0; l < trials_.at(ct).emgData_.at(i).size(); ++l)
-                    cout << trials_.at(ct).emgData_.at(i).at(l) << "\t" ;
-                cout << endl << "LmtTime: " << lmtTime << endl;
-                
-                for(unsigned int l=0; l < trials_.at(ct).lmtData_.at(k).size(); ++l)
-                    cout << trials_.at(ct).lmtData_.at(k).at(l) << "\t";
-                cout << endl << "MomentArms" << endl;
-                
-                for(unsigned int l=0; l < trials_.at(ct).maData_.at(0).at(k).size(); ++l)
-                    cout << trials_.at(ct).maData_.at(0).at(k).at(l) << "\t";
-                cout << "\ncurrent torque: " << currentTorques.at(0);
-        
-                cout << endl << "----------------------------------------" << endl;
-#endif
+    
             }
             
         } 
