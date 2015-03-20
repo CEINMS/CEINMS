a8f3daeef1075e907627b61956abfe87942510a8
Claudio Pizzolato <claudio.pizzolato@griffithuni.edu.au>
2015-03-20 12:07:41 +1000

    Create DataTable class to organise TrialData

---
+++ lib/OptimizerSystems/DataTable.h
@@ -0,0 +1,120 @@
+#ifndef Ceinms_DataTable_h
+#define Ceinms_DataTable_h
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+template<typename T>
+class DataTable {
+
+public:
+    DataTable();
+    DataTable(size_t nRows, size_t nCols); //do not include time column
+    void setLabels(const std::vector<std::string>& labels); //do not include time column
+    void set(T value, size_t row, size_t col);
+    const T& at(size_t row, size_t col) const;
+    T& at(size_t row, size_t col);
+    void pushRow(T time, const std::vector<T>& values);
+    void setRow(size_t row, const std::vector<T>& values);
+
+    std::string getLabel(size_t col) const { return labels_.at(col); }
+    T get(size_t row, size_t col) const { return data_.at(row, col); }
+    T getTime(size_t row) const { return time_.at(row); }
+    T getStartTime() const { return time_.front() }
+    T getFinalTIme() const { return time_.back(); }
+    std::vector<T> getTimeColumn(size_t col) const { return time_; }
+    std::vector<T> getRow(size_t row) const;
+    std::vector<T> getColumn(size_t col) const;
+    size_t getNColumns() const { return nCols_ };
+    size_t getNRows() const { return nRows_ }
+
+private:
+    size_t nRows_, nCols_;
+    //internal implementation may be changed to something better.. e.g. nice matrices
+    std::vector<std::vector<T>> data_;
+    std::vector<T> time_;
+    std::vector<std::string> labels_;
+
+
+};
+
+
+template <typename T>
+DataTable<T>::DataTable() :
+nRows_(0), nCols_(0) {}
+
+template <typename T>
+DataTable<T>::DataTable(size_t nRows, size_t nCols) :
+nRows_(nRows), nCols_(nCols), data_(std::vector<std::vector<T>>(nRows, std::vector<T>(nCols, 0))) {
+
+}
+
+template <typename T>
+void DataTable<T>::setLabels(const std::vector<std::string>& labels) {
+
+    if (!data_.empty() && labels.size() != nCols_)
+        throw std::invalid_argument("wrong number of labels. #labels " + labels.size() + " #columns " + nCols_);
+    labels_ = labels;
+}
+
+
+template <typename T>
+void DataTable<T>::set(T value, size_t row, size_t col) {
+    
+    data_.at(row).at(col) = value;
+}
+
+
+template <typename T>
+const T& DataTable<T>::at(size_t row, size_t col) const {
+
+    return data_.at(row).at(col);
+}
+
+template <typename T>
+T& DataTable<T>::at(size_t row, size_t col) {
+
+    return data_.at(row).at(col);
+}
+
+
+template <typename T>
+void DataTable<T>::pushRow(T time, const std::vector<T>& values){
+
+    if (values.size() != nCols_ && !data_.empty())
+        throw std::invalid_argument("pushRow: Wrong size of values");
+
+    data_.emplace_back(values);
+    nRows_++;
+    nCols_ = values.size();
+    time_.emplace_back(time);
+}
+
+
+template <typename T>
+void DataTable<T>::setRow(size_t row, const std::vector<T>& values) {
+
+    if (values.size() != nCols_)
+        throw std::invalid_argument("setRow: Wrong size of values");
+    data_.at(row) = values;
+    
+}
+
+template <typename T>
+std::vector<T> DataTable<T>::getRow(size_t row) const {
+
+    return data_.at(row);
+}
+
+template <typename T>
+std::vector<T> DataTable<T>::getColumn(size_t col) const {
+
+    std::vector<T> column;
+    column.reserve(nRows_);
+    for (auto& row : data_)
+        column.emplace_back(row.at(col));
+    return column;
+}
+
+
+#endif
--- lib/OptimizerSystems/TrialData.h
+++ lib/OptimizerSystems/TrialData.h
@@ -14,29 +14,21 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include "DataTable.h"
+
+
 
 class TrialData {
 public:
+    std::string id_;
+    DataTable<double> emgData_;
+    DataTable<double> lmtData_;
+    DataTable<double> torqueData_;
 
- // void crop( const double from, const double to);
-  std::string id_; // OK
-  int noMuscles_; // OK
-  int noEmgSteps_; // OK
-  std::vector< std::string > muscleNames_;
-  std::vector< double > emgTimeSteps_;  // OK
-  std::vector< std::vector < double > > emgData_; // OK 
-  int noLmtSteps_; // OK
-  std::vector< double > lmtTimeSteps_; // OK
-  std::vector< std::vector < double > > lmtData_; // OK
-  int noDoF_; // OK
-  std::vector<std::string> dofNames_; // OK 
-  // each ma has a different matrix
-  std::map<std::string, std::vector<std::string> > maMusclesNames_;
-  std::vector< std::vector < std::vector < double > > > maData_; // OK 
-  unsigned int noTorqueSteps_;
-  std::vector< double > torqueTimeSteps_; // OK
-  std::vector< std::vector < double > > torqueData_;
- // friend std::ostream& operator<< (std::ostream& output, const TrialData& b);
+    size_t noDoF_;
+    std::vector<std::string> dofNames_;
+    std::vector<DataTable<double>> maData_;
+
 };
 
 /*
